fbefa5d3e601195380d100ad54f8279da2ca996b
kenpachiii <m.lewis1337@gmail.com>
2018-10-04 12:56:38 -0400

    Updated script to execute correctly

--- ctakes-extras/ctakes-install.sh
+++ ctakes-extras/ctakes-install.sh
@@ -1,77 +1,128 @@
 #!/bin/bash
-#
-# Download and install cTAKES
-#
-# Dependencies:
-# - java
-# - mvn (Maven)
-# - svn (Subversion)
 
 PWD=$(pwd)
 ORIG=$(echo $PWD/$(dirname $0) | sed 's#/\.##')
-SOURCE="$ORIG/../../ctakes-svn"
-TARGET="$SOURCE/../ctakes"
-echo "->  Subversion repo:   $SOURCE"
-echo "->  Install directory: $TARGET"
+CTAKES_DEV_HOME="$ORIG/ctakes"
+CTAKES_SRC_HOME="${CTAKES_DEV_HOME}/trunk"
+printf "\n\u0F36 Subversion repo: \n  $CTAKES_DEV_HOME \n"
+printf "\n\u0F36 Install directory: \n  $CTAKES_SRC_HOME \n"
 
-# warn if we already have an install
-if [ -d "$TARGET" ]; then
-	echo "x>  Install directory already exists, you must remove it manually before we can install cTAKES anew."
+# check for dependencies
+printf "\n\u0F36 Checking for dependencies...\n"
+
+# Java check
+if type -p java 2>&1 >/dev/null; then
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then     
+    _java="$JAVA_HOME/bin/java" 2>&1 >/dev/null
+else
+    printf "\n  \u2573 Java wasn't found. Please install Java 1.8 or greater and try again!"
 	exit 1
 fi
 
-# move to project root (should not hardcode this...)
-cd $SOURCE/..
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    if [[ "$version" > "1.8" ]] || [[ "$version" > "10.0.0" ]]; then
+        printf "\n  \u2713 Java 1.8 or greater is installed!"
+    else         
+        printf "\n  \u2573 Current Java version is $version please upgrade to Java 1.8 or greater!"
+		exit 1
+    fi
+fi
+# Maven check
+mvn --version 2>&1 >/dev/null
+
+if [ 1 == $? ]; then
+	printf "\n  \u2573 Maven isn\'nt installed!\n"
+	exit 1
+else
+	printf "\n  \u2713 Maven is installed!"
+fi
+# Subversion check
+which svn 2>&1 >/dev/null
+
+if [ 1 == $? ]; then
+	printf "\n  \u2573 Subversion isn\'nt installed!\n"
+	exit 1
+else
+	printf "\n  \u2713 Subversion is installed!\n"
+fi
+
+# warn if we already have an install
+if [ -d "$CTAKES_SRC_HOME" ]; then
+	printf "\n\u0F36 cTakes install already exists. Remove it to install again or execute run.sh to start the program."
+	exit 1
+fi
 
 # checkout ctakes from SVN
-if [ ! -d "$SOURCE" ]; then
-	echo "->  Checking out cTAKES from Subversion repo"
-	# svn co https://svn.apache.org/repos/asf/ctakes/trunk "$SOURCE"
-	out_svn=$(svn co https://svn.apache.org/repos/asf/ctakes/tags/ctakes-4.0.0 "$SOURCE")
+if [ ! -d "$CTAKES_DEV_HOME" ]; then
+	mkdir $CTAKES_DEV_HOME
+	printf "\n\u0F36 Checking out cTAKES from Subversion repo... \n"
+	out_svn=$(svn co https://svn.apache.org/repos/asf/ctakes/trunk "$CTAKES_DEV_HOME")
 else
-	echo "->  Updating cTAKES repo"
-	cd "$SOURCE"
+	printf "\n\u0F36 Updating cTAKES repo... \n"
+	cd $CTAKES_DEV_HOME
 	out_svn=$(svn up)
 	cd ..
 fi
 
 if [ 1 == $? ]; then
-	echo "x>  Failed to checkout cTAKES, here's Subversion's output:"
+	printf "\n\u2573 Failed to checkout cTAKES, here's Subversion's output:\n"
 	echo $out_svn
 	exit 1
 fi
 
-# package with Maven
-echo "->  Packaging cTAKES"
-cd "$SOURCE"
-out_mvn=$(mvn package)
 
-if [ 1 == $? ]; then
-	echo "x>  Failed to package cTAKES, here's Maven's output:"
-	echo $out_mvn
-	exit 1
+if [ ! -d "${CTAKES_SRC_HOME}" ]; then
+	mkdir ${CTAKES_SRC_HOME}
+	cd ${CTAKES_SRC_HOME}
+
+	printf "\n\u0F36 Fetching cTAKES git source...\n"
+	git init .
+	git remote add apache https://github.com/apache/ctakes.git
+	git fetch apache
+	git pull apache trunk
+
+	printf "\n\u0F36 Rebasing after svn checkin...\n"
+	git checkout trunk
+	svn update
+	git checkout .
+	git pull --rebase apache trunk
+
 fi
 
-# extract and move built products into place
-echo "->  Moving cTAKES into place"
-base=$(echo ctakes-distribution/target/*-bin.tar.gz)
-tar xzf $base
-mv $(basename ${base%-bin.tar.gz}) "$TARGET"
+# # package with Maven
+# printf "\n\u0F36 Packaging cTAKES with Maven...\n"
+# cd "$CTAKES_DEV_HOME"
+# out_mvn=$(mvn package) 
 
-# download resources
-cd "$TARGET"
-if [ -z ctakes-resources.zip ]; then
-	echo "->  Downloading cTAKES resources"
-	curl -o ctakes-resources.zip "http://hivelocity.dl.sourceforge.net/project/ctakesresources/ctakes-resources-4.0.0.zip"
-	tar xzf ctakes-resources.zip
-fi
+# if [ 1 == $? ]; then
+# 	printf "\n\u2573 Failed to package cTAKES, here's Maven's output:\n"
+# 	echo $out_mvn
+# 	exit 1
+# fi
 
-# add special classes and scripts
-echo "->  Adding special files"
-cp "$ORIG/run.sh" "$TARGET/"
-cp "$ORIG/log4j.xml" "$TARGET/config/"
-cp "$ORIG"/*.class "$TARGET/"
-cp "$ORIG"/*.java "$TARGET/"
-cp "$ORIG"/*.xml "$TARGET/"
+# # extract and move built products into place
+# printf "\n\u0F36  Moving cTAKES to $CTAKES_SRC_HOME...\n"
+# base=$(echo ctakes-distribution/CTAKES_SRC_HOME/*-bin.tar.gz)
+# tar xzf $base
+# mv $(basename ${base%-bin.tar.gz}) "$CTAKES_SRC_HOME"
 
-echo "->  Done"
+# # download resources
+# cd "$CTAKES_SRC_HOME"
+# if [ -z ctakes-resources.zip ]; then
+# 	printf "\n\u0F36  Downloading cTAKES resources...\n"
+# 	curl -o ctakes-resources.zip "http://hivelocity.dl.sourceforge.net/project/ctakesresources/ctakes-resources-4.0.0.zip"
+# 	tar xzf ctakes-resources.zip
+# fi
+
+# # add special classes and scripts
+# printf "\n\u0F36  Adding special files...\n"
+# cp "$ORIG/run.sh" "$CTAKES_SRC_HOME/"
+# cp "$ORIG/log4j.xml" "$CTAKES_SRC_HOME/config/"
+# cp "$ORIG"/*.class "$CTAKES_SRC_HOME/"
+# cp "$ORIG"/*.java "$CTAKES_SRC_HOME/"
+# cp "$ORIG"/*.xml "$CTAKES_SRC_HOME/"
+
+printf "\n\u0FC9>  Done!"
+
