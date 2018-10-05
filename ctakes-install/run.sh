ba8ed140c5b3c6a902ba9a9bd118b204bf71e99d
kenpachiii <m.lewis1337@gmail.com>
2018-10-05 18:43:20 -0400

    Updated launch correctly

--- ctakes-install/run.sh
+++ ctakes-install/run.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 #
-# Requires JAVA JDK 1.6+
+# Requires JAVA JDK 1.8+
 
-# source UMLS credentials
-if [ -z ./umls.sh ]; then
-  echo "You need to provide UMLS credentials in the file ./umls.sh" &>2
-  exit 1
+# Check for UMLS credentials
+if [ ! -f $PWD/ctakes-install/umls.sh ]; then
+    printf "\033[91mERROR:\033[0m You need to provide UMLS credentials in the file ./umls.sh" 1>&2
+	exit 1
+else
+# Source UMLS credentials
+	printf "\033[92m\u0F36\033[0m UMLS credentials file confirmed!\n\n"
+	. ./ctakes-install/umls.sh
 fi
-. ./umls.sh
 
-# only set CTAKES_HOME if not already set
-[ -z "$CTAKES_HOME" ] && CTAKES_HOME=$(dirname $0)
+# Only set CTAKES_HOME if not already set
+[ -z "$CTAKES_HOME" ] && CTAKES_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/apache-ctakes-4.0.0"
 cd $CTAKES_HOME
 
-# launch
-java -cp $CTAKES_HOME:$CTAKES_HOME/lib/*:$CTAKES_HOME/desc/:$CTAKES_HOME/resources/ \
-	-Dlog4j.configuration=file:$CTAKES_HOME/config/log4j.xml \
-	-Dctakes.umlsuser=$UMLS_USERNAME -Dctakes.umlspw=$UMLS_PASSWORD \
-	-Xms512M -Xmx1024M \
-	CmdLineCpeRunner FilesToXmi.xml
+# Launch
+
+bin/runClinicalPipeline.sh -i /home/a1d3n/Desktop/cTAKES-Python-API/ctakes-test/ctakes_input  --xmiOut /home/a1d3n/Desktop/cTAKES-Python-API/ctakes-test/ctakes_output --user $UMLS_USERNAME --pass $UMLS_PASSWORD
