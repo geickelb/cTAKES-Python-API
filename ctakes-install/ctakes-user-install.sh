2001a55321d1d64f8c275512f7247a60a34a5657
kenpachiii <m.lewis1337@gmail.com>
2018-10-05 22:35:58 -0400

    Added colored icons

--- ctakes-install/ctakes-user-install.sh
+++ ctakes-install/ctakes-user-install.sh
@@ -8,11 +8,11 @@ PWD=$(pwd)
 ORIG=$(echo $PWD/$(dirname $0) | sed 's#/\.##')
 cTAKES_HOME="$ORIG/apache-ctakes-4.0.0"
 
-printf "\n\u0F36 Install directory: $cTAKES_HOME \n"
+printf "\n\033[92m\u0F36\033[0m Install directory: $cTAKES_HOME \n"
 
 ### Checking for dependencies ###
 
-printf "\n\u0F36 Checking for dependencies...\n"
+printf "\n\033[92m\u0F36\033[0m Checking for dependencies...\n"
 
 # Jave Check #
 
@@ -28,9 +28,9 @@ fi
 if [[ "$_java" ]]; then
     version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
     if [[ "$version" > "1.8" ]] || [[ "$version" > "10.0.0" ]]; then
-        printf "\n  \u2713 Java 1.8 or greater is installed!\n"
+        printf "\n  \033[92m\u2713\033[0m Java 1.8 or greater is installed!\n"
     else         
-        printf "\n  \u2573 Current Java version is $version please upgrade to Java 1.8 or greater!\n"
+        printf "\n  \033[91m\u2573\033[0m Current Java version is $version please upgrade to Java 1.8 or greater!\n"
 		exit 1
     fi
 fi
@@ -38,14 +38,14 @@ fi
 # Warn if install exists #
 
 if [ -d "$CTAKES_HOME" ]; then
-	printf "\n  \u2573 cTakes install already exists!\n\n"
+	printf "\n  \033[91m\u2573\033[0m cTakes install already exists!\n\n"
 	exit 1
 fi
 
 # Download cTAKES user install file linux #
-
+#FIXME: Progress bar isn\'t displaying'
 if [ ! -d "CTAKES_HOME" ]; then
-	printf "\n\u0F36 Downloading cTAKES from Apache repo...\n\n"
+	printf "\n\033[92m\u0F36\033[0m Downloading cTAKES from Apache repo...\n\n"
 
     wget --progress=bar:force -o /dev/null http://www-eu.apache.org/dist/ctakes/ctakes-4.0.0/apache-ctakes-4.0.0-bin.tar.gz -P $ORIG/tmp/ 2>&1 | tail -f -n +6
     tar -xvf $ORIG/tmp/apache-ctakes-4.0.0-bin.tar.gz -C "$ORIG/$CTAKES_HOME"
@@ -53,11 +53,11 @@ fi
 
 # Get resource files #
 
-printf "\n\u0F36 Getting cTAKES resource files...\n\n"
+printf "\n\033[92m\u0F36\033[0m Getting cTAKES resource files...\n\n"
 cd $ORIG/tmp
 wget --progress=bar:force -o /dev/null http://sourceforge.net/projects/ctakesresources/files/ctakes-resources-4.0-bin.zip 2>&1 | tail -f -n +6
 
-printf "\u0F36 Unzipping and moving resource files...\n\n"
+printf "\033[92m\u0F36\033[0m Unzipping and moving resource files...\n\n"
 unzip ctakes-resources-4.0-bin.zip
 cp -R $ORIG/tmp/resources/* $ORIG/apache-ctakes-4.0.0/resources
 rm -r $ORIG/tmp/
@@ -96,9 +96,9 @@ if [ ! -f $PWD/umls.sh ]; then
             fi
         }
         set_password
-        printf "\n\u0F36 UMLS credentials updated!\n"
+        printf "\n\033[92m\u0F36\033[0m UMLS credentials updated!\n"
     else
-        printf "\n\u0F36 No worries you can add them manually later!\n"
+        printf "\n\033[92m\u0F36\033[0m No worries you can add them manually later!\n"
     fi
 fi
 printf "\n\u0FC9 DONE!\n\n"
